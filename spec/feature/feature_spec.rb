15d57f148b14bc5155241d625f78dbaeff162617
Karla <karla.gardiner@gmail.com>
2020-04-22 14:17:19 +0100

    Feature test: wrote test that withdrawing money won't raise error

--- spec/feature/feature_spec.rb
+++ spec/feature/feature_spec.rb
@@ -7,8 +7,15 @@ describe 'User Stories' do
   # As a user
   # So I can save my money
   # I would like to deposit money into my account
-  it 'so I can save money, deposit money into my account' do
+  it 'so money can be saved, deposit money into account' do
     expect { account.deposit(1000) }.not_to raise_error
+  end
+
+  # As a user
+  # So I can use the money I've saved
+  # I would like be able to withdraw money in my account
+  it 'so money can be used, withdraw money from account' do
+    expect { account.withdraw(200) }.not_to raise_error
   end
 
 end
